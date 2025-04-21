0779279a2e8c145d6a557de36a16a6e5a6eee640
Yash-Varmora <yash.varmora@bacancy.com>
2025-04-21 11:32:02 +0530

    test husky hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 
 const App = () => {
-  return <div>
-    <h1>
-      Hello
-    </h1>
-    <h5>dhvbahbv</h5>
-    <h4>sdhvwjhvd</h4>
-                 <h2>
-                                                  hi...
-                         </h2>
-  </div>;
+  return (
+    <div>
+      <h1>Hello</h1>
+      <h5>dhvbahbv</h5>
+
+      <h4>sdhvwjhvd</h4>
+
+      <h2>hi...</h2>
+
+      <h2>jbgarjbg</h2>
+    </div>
+  );
 };
 
 export default App;
